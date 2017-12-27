49c194a9a581bfd86b08600bb2028d904ea826c4
Hamidin Hidayat <mailnidimah.hamidin@gmail.com>
2017-12-27 16:09:08 +0700

    Vue Scaffolding Example

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,22 +1,48 @@
+//
+// /**
+//  * First we will load all of this project's JavaScript dependencies which
+//  * includes Vue and other libraries. It is a great starting point when
+//  * building robust, powerful web applications using Vue and Laravel.
+//  */
+//
+// require('./bootstrap');
+//
+// window.Vue = require('vue');
+//
+// /**
+//  * Next, we will create a fresh Vue application instance and attach it to
+//  * the page. Then, you may begin adding components to this application
+//  * or customize the JavaScript scaffolding to fit your unique needs.
+//  */
+//
+// Vue.component('example-component', require('./components/ExampleComponent.vue'));
+//
+// const app = new Vue({
+//     el: '#app'
+// });
 
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
+import Vue from 'vue';
 
-require('./bootstrap');
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
 
-window.Vue = require('vue');
+import VueAxios from 'vue-axios';
+import axios from 'axios';
+Vue.use(VueAxios, axios);
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
+// const router = new VueRouter({ mode: 'history'});
+// new Vue(Vue.util.extend({ router })).$mount('#app');
+import Example from './components/ExampleComponent.vue';
+const routes = [
+  {
+      name: 'Example',
+      path: '/',
+      component: Example
+  }
+];
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
+const router = new VueRouter({ mode: 'history', routes: routes});
 
-const app = new Vue({
-    el: '#app'
-});
+import App from './App.vue';
+
+new Vue(Vue.util.extend({ router }, App)).$mount('#app');
